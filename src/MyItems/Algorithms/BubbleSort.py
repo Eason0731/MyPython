ddda2e5de166fc906ec03e698d7d099684d3b784
john0731 <john.zhang0731@outlook.com>
2018-03-12 09:45:42 +0800

    Update  the type of list number from str to int when added to List on BubbleSort

--- src/MyItems/Algorithms/BubbleSort.py
+++ src/MyItems/Algorithms/BubbleSort.py
@@ -1,7 +1,6 @@
 import os
 
 def BubbleSortbyASC(List):
-    #List = [21,8,5,1,7,3,14]  
     for i in range(0,len(List)):
         for j in range(i+1,len(List)):
             if List[i] > List[j]:
@@ -12,7 +11,6 @@ def BubbleSortbyASC(List):
     print ("The result of bubble sort order by ASC is: " + str(List))
     
 def BubbleSortbyDESC(List):
-    #List = [21,8,5,1,7,3,14]  
     for i in range(0,len(List)):
         for j in range(i+1,len(List)):
             if List[i] < List[j]:
@@ -22,22 +20,20 @@ def BubbleSortbyDESC(List):
 
     print ("The result of bubble sort order by DESC is: " + str(List))
 
-def BubbleSortbyCutsom(num):
+def BubbleSortbyCutsom(Number):
     List = []
-    for i in range(0,int(num)):
+    for i in range(0,int(Number)):
         ListNum = input("Please input the number " + str(i+1)+ ":")
         if (ListNum.startswith('-') and ListNum[1:] or ListNum).isdigit():
-            List.append(ListNum)
+            List.append(int(ListNum))
         else:
             print (ListNum + " is a illegeal number and won't add it to the list!")
-    print (List)
     BubbleSortbyASC(List)
     BubbleSortbyDESC(List)
     
 if __name__ == '__main__':
-    num = input("How many numbers do you want to input?")
-    if num.isdigit():
-        BubbleSortbyCutsom(num)
+    Number = input("How many numbers do you want to input?")
+    if Number.isdigit():
+        BubbleSortbyCutsom(Number)
     else:
         print ("Cannot input illegal number ,please input again!")
-            
