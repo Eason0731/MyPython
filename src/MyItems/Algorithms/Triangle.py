16d813fb9f58b2a7bf7fb93ecd1b75228d238f02
john0731 <john.zhang0731@outlook.com>
2018-03-14 09:34:57 +0800

    Update the hint message from Chinese to English on Triangle.py

--- src/MyItems/Algorithms/Triangle.py
+++ src/MyItems/Algorithms/Triangle.py
@@ -3,9 +3,9 @@ import re
 
 def MainFunction():
     while(1):
-        A = input ("请输入第一条边: ")
-        B = input ("请输入第二条边: ")
-        C = input ("请输入第三条边: ")
+        A = input ("Please input the first edge: ")
+        B = input ("Please input the second edge: ")
+        C = input ("Please input the third edge:: ")
     
         Regex = re.compile(r"^(-?\d+)(\.\d*)?$")
         if re.match(Regex,A) and re.match(Regex,B) and re.match(Regex,C):
@@ -13,27 +13,27 @@ def MainFunction():
                 Triangle(float(A),float(B),float(C))
                 break
             else:
-                print ("边不能负数,请重新输入!")
+                print ("The value of edge cannot be negative number ,please input again!")
                 print ("                             ")
         else:
-            print ("其中有边的值不是数字,请重新输入!")
+            print ("The value of edge is not a number,please input again!")
             print ("                             ")
 
 
 def Triangle(A,B,C):
     if A+B>C and A+C>B and B+C>A:
         if A==B==C:
-            print ("这是个等边三角形")
+            print ("This is an equilateral triangle")
         elif A==B or A==C or B==C:
-            print ("这是个等腰三角形")
+            print ("This is an isosceles triangle")
         elif pow(A,2)+pow(B,2)==pow(C,2) or pow(A,2)+pow(C,2)==pow(B,2) or pow(B,2)+pow(C,2)==pow(A,2):
-            print ("这是个直角三角形")
-        elif A== B and pow(A,2)+pow(B,2)==pow(C,2) or A==C and pow(A,2)+pow(C,2)==pow(B,2) or B==C and pow(B,2)+pow(C,2)==pow(A,2):
-            print ("这是个等腰直角三角形")
+            print ("This is a right triangle")
+        elif (A== B and pow(A,2)+pow(B,2)==pow(C,2)) or (A==C and pow(A,2)+pow(C,2)==pow(B,2)) or (B==C and pow(B,2)+pow(C,2)==pow(A,2)):
+            print ("This is an isosceles right angle triangle")
         else:
-            print ("这是个普通三角形")
+            print ("This is an ordinary triangle")
     else:
-        print ("无法构成三角形")
+        print ("Cannot build a triangle")
 
 if __name__ == '__main__':
     MainFunction()
