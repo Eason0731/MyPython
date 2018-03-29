4860a2445d60a9453baa59ed8f61fb1c443fbdac
john0731 <john.zhang0731@outlook.com>
2018-03-29 09:22:50 +0800

    Add some dishes names of list on ChooseLunch.py

--- src/MyItems/Tools/ChooseLunch.py
+++ src/MyItems/Tools/ChooseLunch.py
@@ -19,11 +19,12 @@ Choose number of people:
             print ("Do not input illegal number, please input again!")
 
 def ChooseLunch(People):
-    List1 = ['老广东','福荣祥烧腊','吉祥馄饨','原素Essence西餐','杨铭宇黄焖鸡米饭','东池便当','大阪牛肉饭','福客桂林米粉','杨国富麻辣烫']       
+    List1 = ['老广东','福荣祥烧腊','吉祥馄饨','原素Essence西餐','杨铭宇黄焖鸡米饭','东池便当','大阪牛肉饭','福客桂林米粉','杨国富麻辣烫','饭戒','文庙菜饭','羊肉泡馍']       
     if 'M' in People:
         List1.remove('原素Essence西餐')
         List1.append('蜀道麻辣香锅')
         List1.append('小四川')
+        List1.append('美味小厨')
 
     print ("We decided that today's lunch is .....")
     random.shuffle(List1)
