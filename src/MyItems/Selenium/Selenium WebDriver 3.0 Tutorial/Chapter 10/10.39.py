f363afe01e5d111c0c4c29de34b6a9e61fd3c38d
john0731 <john.zhang0731@outlook.com>
2018-06-24 11:28:55 +0800

    Add demo 10.39 - IdentifyNewPopUpWindowByTitle

---
+++ src/MyItems/Selenium/Selenium WebDriver 3.0 Tutorial/Chapter 10/10.39.py
@@ -0,0 +1,54 @@
+import os
+import unittest
+import Browser
+import time
+
+import traceback #引入堆栈类
+from selenium.webdriver.common.by import By #导入By类
+from selenium.webdriver.support.ui import WebDriverWait #导入显式等待类
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException,NoSuchElementException #导入期望场景类
+
+class IdentifyNewPopUpWindowByTitle(unittest.TestCase):
+    def setUp(self):
+        self.driver = Browser.Chrome()
+
+    def testIdentifyNewPopUpWindowByTitle(self):
+        driver = self.driver
+        WebSite = os.path.join(os.path.abspath('..'),'Html','10.39.html')
+        driver.get(WebSite)
+        time.sleep(2) #等待2秒
+        
+        WebDriverWait(driver,10,0.2).until(EC.element_to_be_clickable((By.LINK_TEXT,'Baidu Search'))).click() #WebDriverWait是设置显式等待的方法,这里设置10秒等待时间,使用element_to_be_clickable()方法判断页面为Baidu Search的链接是否可被单击，并点击
+        time.sleep(2)
+        
+        All_Handles = driver.window_handles #使用window_handles方法，获取当前所有打开浏览器的窗口句柄
+        time.sleep(2)
+
+        print (driver.current_window_handle) #使用current_window_handle方法，输出打印当前浏览器的窗口句柄
+        time.sleep(2)
+
+        print (len(All_Handles)) #输出打印打开浏览器的个数
+        time.sleep(2)
+
+        if len(All_Handles) > 0:
+            try:
+                for WindowHandle in All_Handles:
+                    driver.switch_to_window(WindowHandle)
+                    print (driver.title)
+                    if '百度一下，你就知道' == driver.title:
+                        WebDriverWait(driver,10,0.2).until(lambda x:x.find_element_by_id('kw')).send_keys('百度首页的窗口被找到了!') #WebDriverWait是设置显式等待的方法,这里设置10秒等待时间,并找到百度搜索框，输入所期望的内容
+                        time.sleep(2)
+                        
+            except TimeoutException as e:
+                print (traceback.print_exc())
+            except NoSuchElementException as e:
+                print (traceback.print_exc())
+            except Exception as e:
+                print (traceback.print_exc())
+        
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main()
