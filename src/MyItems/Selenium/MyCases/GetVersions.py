8c6d2c271b19fd3d1044c05ab426430a961a4d93
john0731 <john.zhang0731@outlook.com>
2018-07-24 14:57:30 +0800

    Add function GetGitVersion on GetVersion.py

---
+++ src/MyItems/Selenium/MyCases/GetVersions.py
@@ -0,0 +1,47 @@
+import os,time,unittest,Browser
+
+class GetVersions(unittest.TestCase):
+    def setUp(self):
+        self.driver = Browser.Chrome()
+    
+    def testGetChromeDriverVersion(self):
+        driver = self.driver
+        driver.get("https://sites.google.com/a/chromium.org/chromedriver/")
+        time.sleep(3)
+
+        ChromeDriverVersion = driver.find_element_by_xpath('//*[@id="sites-canvas-main-content"]/table/tbody/tr/td/div/h2/font/a')
+        print (ChromeDriverVersion.text)
+        time.sleep(3)
+
+    def testGetEdgeDriverVersion(self):
+        driver = self.driver
+        driver.get("https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/")
+        time.sleep(3)
+
+        EdgeDriverVersion = driver.find_element_by_xpath('//*[@id="downloads"]/div/div[2]/ul/li[2]/a')
+        print (EdgeDriverVersion.text)
+        time.sleep(3)
+    
+    def testGetGeckoDriverVersion(self):
+        driver = self.driver
+        driver.get("https://github.com/mozilla/geckodriver/releases")
+        time.sleep(3)
+
+        GeckoDriverVersion = driver.find_element_by_xpath('//*[@id="js-repo-pjax-container"]/div[2]/div[1]/div[2]/div[1]/div[2]/div[1]/h1/a')
+        print ("Latest Gecko driver version is: " + GeckoDriverVersion.text)
+        time.sleep(3)
+    
+    def testGetGitVersion(self):
+        driver = self.driver
+        driver.get("https://git-scm.com/")
+        time.sleep(3)
+
+        GitVersion = driver.find_element_by_xpath('//*[@id="front-downloads"]/div/span[1]')
+        print ("Latest Git version is: " + GitVersion.text)
+        time.sleep(3)
+    
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main()
