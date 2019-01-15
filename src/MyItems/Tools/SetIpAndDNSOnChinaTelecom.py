274d87fe378f21e8a86df0580f51a38a14636f68
Eason0731 <Eason.Zhang0731@outlook.com>
2019-01-15 09:21:43 +0800

    Update some codes on SetIpAndDNSOnChinaTelecom.py

--- src/MyItems/Tools/SetIpAndDNSOnChinaTelecom.py
+++ src/MyItems/Tools/SetIpAndDNSOnChinaTelecom.py
@@ -5,51 +5,12 @@ wmiService = wmi.WMI()
 colNicConfigs = wmiService.Win32_NetworkAdapterConfiguration(IPEnabled=True)
 objNicConfig = colNicConfigs[0]
 
-def SetOnRoom401():
+def SetIPandDNS(IP,Yanma,Gateway,DNSServers):
     if len(colNicConfigs) < 1:
         #print("没有找到可用的网络适配器")
         print ("No available network adapters found!")
         exit()
-    IP = ['10.6.246.85']
-    Yanma = ['255.0.0.0']
-    Gateway = ['10.6.246.1']
-    DNSServers = ['10.5.22.66','10.5.22.67']
-    returnIPValue = objNicConfig.EnableStatic(IPAddress=IP, SubnetMask=Yanma)
-    returnGatewayValue = objNicConfig.SetGateways(DefaultIPGateway=Gateway)
-    returnDNSValue = objNicConfig.SetDNSServerSearchOrder(DNSServerSearchOrder=DNSServers)
-
-    if returnIPValue[0] == 0:
-        print("Set IP and SubnetMask successfully!")
-        print("The new IP address is:" + '  '.join(IP))
-        print("The new SubnetMask address is:" + '  '.join(Yanma))
-        print ("================================================")
-    else:
-        print ("Set IP and SubnetMask Fail!")
-
-    if returnGatewayValue[0] == 0:
-        print("Set Gateway successfully!")
-        print("The new Gateway address is:" + '  '.join(Gateway))
-        print ("================================================")
-    else:
-        print ("Set Gateway Fail!")
-
-    if returnDNSValue[0] == 0:
-        print("Set DNS successfully!")
-        print("The new DNS address is:" + '  '.join(DNSServers))
-        print ("================================================")
-    else:
-        print ("Set DNS Fail!")
-    os.system('IPCONFIG/FLUSHDNS')
-
-def SetOnRoom607():
-    if len(colNicConfigs) < 1:
-        #print("没有找到可用的网络适配器")
-        print ("No available network adapters found!")
-        exit()
-    IP = ['10.6.228.10']
-    Yanma = ['255.255.255.128']
-    Gateway = ['10.6.228.1']
-    DNSServers = ['10.5.22.67']
+    
     returnIPValue = objNicConfig.EnableStatic(IPAddress=IP, SubnetMask=Yanma)
     returnGatewayValue = objNicConfig.SetGateways(DefaultIPGateway=Gateway)
     returnDNSValue = objNicConfig.SetDNSServerSearchOrder(DNSServerSearchOrder=DNSServers)
@@ -78,6 +39,6 @@ def SetOnRoom607():
     os.system('IPCONFIG/FLUSHDNS')
 
 if __name__ == '__main__':
-    #SetOnRoom401()
-    SetOnRoom607()
+    SetIPandDNS(['10.6.228.10'],['255.255.255.128'],['10.6.228.1'],['10.5.22.67'])
+    #SetIPandDNS(['10.6.246.85'],['255.0.0.0'],['10.6.246.1'],['10.5.22.66','10.5.22.67'])
     
