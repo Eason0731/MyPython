0f0b2dbd1cc61129a8e86bd29099f6a70be56107
Eason0731 <Eason.Zhang0731@outlook.com>
2019-01-23 10:36:41 +0800

    Fix #13 issue

--- src/MyItems/Tools/PipInstall.py
+++ src/MyItems/Tools/PipInstall.py
@@ -20,28 +20,65 @@ def PipInstall():
     ,'WMI']
     print ("==========================================================")
     print(time.strftime("Start time :%Y-%m-%d %X",time.localtime()))
+    Install = 0
+    InstallList = []
+    
+    Installed = 0
+    InstalledList = []
+    
+    Updated = 0
+    UpdatedList = []
+    
     for Package in List:
-        InstallList = os.popen('pip list')
-        UpdateList = os.popen('pip list --outdate')
-        if Package not in InstallList.read():
+        InstallPackagesList = os.popen('pip list')
+        UpdatePackagesList = os.popen('pip list --outdate')
+        if Package not in InstallPackagesList.read():
             os.system('pip install ' + Package)
+            Install = Install + 1
+            InstallList.append(Package)
         else:
             print (Package + ' has installed')
+            Installed = Installed + 1
+            InstalledList.append(Package)
         print ("                       ")
-        if Package in UpdateList.read():
+        if Package in UpdatePackagesList.read():
             os.system('pip install '+ Package + ' -U')
+            Updated = Updated + 1
+            UpdatedList.append(Package)
         else:
             print ('No need to update ' + Package)
+        
         print ("==========================================================")
     print(time.strftime("End time :%Y-%m-%d %X",time.localtime()))
     print ("==========================================================")
-    print ("                                                          ")
+    if Install == 0:
+        if Installed == 1:
+            pkg = 'package was'
+        else:
+            pkg = 'packages were'
+        print ("There are {0} ({1}) ".format(str(Installed),','.join(InstalledList)) + pkg + " insalled on this PC!")
+    else:
+        if Install == 1:
+            pkg = 'package has'
+        else:
+            pkg = 'packages have'
+        print ("{0} ({1}) ".format(str(Install),','.join(InstallList)) + pkg +" insalled on this PC successfully!")
+    print ("                                                          ")  
     print (os.popen('pip list').read())
     print ("==========================================================")
-    if '' in os.popen('pip list --outdate').read():
-        print ("All of the installed packages are the latest version!")
+    if Updated == 0:
+        if Installed == 1:
+            pkg = 'package was'
+        else:
+            pkg = 'packages were'
+        print ("{0} ({1}) ".format(str(Installed),','.join(InstalledList)) + pkg + " installed on this PC are the latest version!")
     else:
-        print (os.popen('pip list --outdate').read())
+        if Updated == 1:
+            pkg = 'package has'
+        else:
+            pkg = 'packages have'
+        print ("{0} ({1}) ".format(str(Updated),','.join(UpdatedList)) + pkg + " updated on this PC successfully!")
+    print (os.popen('pip list --outdate').read())
     print ("==========================================================")
 
 if __name__ == '__main__':
