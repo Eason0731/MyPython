41d2f5810721d833916efa98640d498df7a9861f
Eason0731 <Eason.Zhang0731@outlook.com>
2019-01-24 09:22:22 +0800

    Fix #12 issue

--- src/MyItems/Tools/BackupArchiveFiles.py
+++ src/MyItems/Tools/BackupArchiveFiles.py
@@ -184,7 +184,7 @@ def MyFiles(source2Kfolder,sourcePES,sourceTDU,sourceTencentFiles,sourceBusDrive
         if not os.listdir(BackupFolder):
             shutil.rmtree(BackupFolder)
             if not os.path.exists(BackupFolder):
-                print("All archive files has been put back")
+                print("All archive files have been put back")
                 print("Backup folder " + BackupFolder + " has been deleted successfully!")
                 print("                                ")
     print(time.strftime("End time :%Y-%m-%d %X",time.localtime()))
