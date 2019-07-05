f0a734aed6515ea8417cafe3a73f1ec59631d56b
Eason0731 <Eason.Zhang0731@outlook.com>
2019-07-05 14:49:54 +0800

    Add CRMItemsFormat.py to project

---
+++ src/MyItems/Tools/CRMItemsFormat.py
@@ -0,0 +1,20 @@
+import os
+
+FList = ['2-DUI4856'
+,'2-DUI485W'
+,'2-DUI487X'
+,'2-DUI4894'
+,'2-DUI48AN'
+,'2-DUI48B9'
+,'2-DUI48EF'
+,'2-DUI48EX'
+,'2-DUI48FL'
+,'2-DUI48FM'
+,'2-DUI48GJ'
+,'2-DUI48GN']
+
+
+for ItemsNum in FList:
+    print ("|"+ItemsNum ,end='')
+    
+print ("|")
