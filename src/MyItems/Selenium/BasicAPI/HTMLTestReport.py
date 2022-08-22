421d76d6c24cf21254fd14e56b421b7aa77ff539
Eason0731 <Eason.Zhang0731@outlook.com>
2022-08-22 10:26:46 +0800

    Fix the time bug on HTMLTestRunner.py again

--- src/MyItems/Selenium/BasicAPI/HTMLTestReport.py
+++ src/MyItems/Selenium/BasicAPI/HTMLTestReport.py
@@ -8,13 +8,13 @@ def HTMLTestReport():
     Today = time.strftime("%Y%m%d")
     ReportResultDir = os.path.join('E:\\','Test Report','Selenium Test',Today)
     
-    now = time.strftime("%Y-%m-%d %H-%M-%S") # 按照一定的格式获取当前的时间
-    Execute_Time = time.strftime("%Y-%m-%d %H:%M:%S") # 将执行时间传给邮件主题
-    #filename = ReportResultDir + now + '_API_Test_Result.html' 
+    #now = time.strftime("%Y-%m-%d %H-%M-%S") # 按照一定的格式获取当前的时间
+    Execute_Time = time.strftime("%Y-%m-%d %H：%M：%S") # 按照一定的格式获取当前的时间，并将执行时间传给邮件主题
+    #filename = ReportResultDir + Execute_Time + '_API_Test_Result.html' 
     if not os.path.exists(os.path.join(ReportResultDir)):
         os.makedirs(os.path.join(ReportResultDir))
         
-    filename = os.path.join(ReportResultDir,now)+ '_Selenium_Test_Result.html'  # 定义报告存放路径 目前是存在运行的当前目录
+    filename = os.path.join(ReportResultDir,Execute_Time)+ '_Selenium_Test_Result.html'  # 定义报告存放路径 目前是存在运行的当前目录
     fp = open(filename, "wb") # 打开file文件流
     
     runner = HTMLTestRunner.HTMLTestRunner(stream=fp,title='Selenium回归测试报告',description='测试用例执行情况 by Eason') # 定义测试报告 
